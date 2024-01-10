23385a895d7eef82d3bad4e1ddc592bc0e2c0973
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-01-10 22:14:14 +0900

    Solve LeetCode Problem - DFS - 951. Flip Equivalent Binary Trees(read solution)

--- DFS/951_Flip_Equivalent_Binary_Trees.h
+++ DFS/951_Flip_Equivalent_Binary_Trees.h
@@ -74,3 +74,60 @@ public:
 
 	}
 };
+
+class Solution {
+	public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+		if (root1 == root2)
+			return true;
+		if (root1 == null || root2 == null || root1.val != root2.val)
+			return false;
+
+		return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right) ||
+			flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+	}
+}
+
+class Solution {
+
+	//check the value of two childs are same or not
+
+public:
+	bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+
+		//if both are nullptr
+		if (!root1 && !root2)
+			return true;
+
+		if (nullptr == root1)
+		{
+			return false;
+		}
+
+		if (nullptr == root2)
+		{
+			return false;
+		}
+
+		if (root1->val != root2->val)
+		{
+			return false;
+		}
+
+		bool r1 = flipEquiv(root1->left, root2->left);
+		bool r2 = flipEquiv(root1->right, root2->right);
+
+		if (r1 && r2)
+		{
+			return true;
+		}
+
+		bool r3 = flipEquiv(root1->left, root2->right);
+		bool r4 = flipEquiv(root1->right, root2->left);
+		if (r3 && r4)
+		{
+			return true;
+		}
+
+		return false;
+	}
+};
