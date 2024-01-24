4f7624c6cd8f49a5ea2e15a67f8a9a7af57711f0
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-01-24 21:57:48 +0900

    Solve LeetCode Problem - Stack - 143. Reorder List(read solution) https://leetcode.com/problems/reorder-list/solutions/1640556/c-easy-to-solve-beginner-friendly-with-detailed-explanation-and-dry-run/
    https://leetcode.com/problems/reorder-list/solutions/2045022/c-stack-easy-understanding/

--- TwoPointers/80_Remove_Duplicates_from_Sorted_Array_II.h
+++ TwoPointers/80_Remove_Duplicates_from_Sorted_Array_II.h
@@ -1,40 +1,88 @@
+//Upvote and Comment
 class Solution {
 public:
-	int removeDuplicates(vector<int>& nums) {
+	void reorderList(ListNode* head) {
+		// base case : linkedlist is empty
+		if (!head) return;
 
-		if(nums.size() == 1)
-		{
-			return 1; 
+		// finding the middle with the help of two pointer approach
+		ListNode* tmp = head, * half = head, * prev = NULL;
+		while (tmp->next && tmp->next->next) {
+			tmp = tmp->next->next;
+			half = half->next;
 		}
 
-		int initNumsSize = nums.size();
-		int pre = nums[0];
-		int count = 1;
-		int eraseCount = 0;
+		// adding one bit in case of lists with even length
+		if (tmp->next) half = half->next;
 
-		for (int i = 1; i < nums.size(); i++)
-		{
-			if (pre == nums[i])
-			{
-				if(2 == count)
-				{
-					nums.erase(nums.begin() + i);
-					eraseCount++ ;
-					i--;
-				}
-				else
-				{
-					count++;
-				}
-			}
-			else
-			{
-				pre = nums[i];
-				count = 1;
-			}
+		// Now reverse the second half
+		while (half) {
+			tmp = half->next;
+			half->next = prev;
+			prev = half;
+			half = tmp;
+		}
+		half = prev;
+
+		// After reversing the second half, let's merge both the halfes
+		while (head && half) {
+			tmp = head->next;
+			prev = half->next;
+			head->next = half;
+			half->next = tmp;
+			head = tmp;
+			half = prev;
 		}
 
-		return initNumsSize - eraseCount;
+		// Base case : closing when we had even length arrays
+		if (head && head->next) head->next->next = NULL;
+	}
+};
+
+
+//https://leetcode.com/problems/reorder-list/solutions/1640556/c-easy-to-solve-beginner-friendly-with-detailed-explanation-and-dry-run/
+
+
+//stack
+class Solution {
+public:
+	void reorderList(ListNode* head) {
+		if (head == NULL || head->next == NULL || head->next->next == NULL) return;
+		ListNode* slow = head;
+		ListNode* fast = head;
+		stack<ListNode*> st;
+		while (fast != NULL && fast->next != NULL) {
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+		ListNode* n;
+
+		if (fast != NULL) {
+			n = slow->next;
+
+		}
+		else n = slow;
+
+		while (n != NULL) {
+			st.push(n);
+			n = n->next;
+
+		}
+		fast = head;
+
+		while (st.size()) {
+
+			n = fast->next;
+			fast->next = st.top();
+			st.top()->next = n;
+			fast = n;
+			st.pop();
+
+		}
+		slow->next = NULL;
+
 
 	}
 };
+
+https://leetcode.com/problems/reorder-list/solutions/2045022/c-stack-easy-understanding/
