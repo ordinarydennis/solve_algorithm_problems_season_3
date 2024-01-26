2e73766541a05841988bab848e4ef19c39701a05
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-01-26 14:09:14 +0900

    Solve LeetCode Problem - Graph - 1584. Min Cost to Connect All Points(read solution)
    https://leetcode.com/problems/min-cost-to-connect-all-points/solutions/1982888/prim-s-algorithm-cpp-solution-with-explanation/
    https://kbj96.tistory.com/15
    https://8iggy.tistory.com/159

--- Graph/1584_Min_Cost_to_Connect_All_Points.h
+++ Graph/1584_Min_Cost_to_Connect_All_Points.h
@@ -46,3 +46,91 @@ public:
 
 	}
 };
+
+int manhattan_distance(vector<int>& p1, vector<int>& p2) {
+	return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]);
+}
+
+class Solution {
+public:
+	int minCostConnectPoints(vector<vector<int>>& points) {
+		int n = points.size();
+		vector<bool> visited(n, false);
+		unordered_map<int, int> heap_dict;
+		for (int i = 0; i < n; ++i) {
+			heap_dict[i] = INT_MAX; // Initialize all distances to infinity
+		}
+		heap_dict[0] = 0; // Start node
+
+		auto cmp = [](pair<int, int> left, pair<int, int> right) { return left.first > right.first; };
+		priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> min_heap(cmp);
+		min_heap.push({ 0, 0 });
+
+		int mst_weight = 0;
+
+		while (!min_heap.empty()) {
+			auto [w, u] = min_heap.top();
+			min_heap.pop();
+
+			if (visited[u] || heap_dict[u] < w) continue;
+
+			visited[u] = true;
+			mst_weight += w;
+
+			for (int v = 0; v < n; ++v) {
+				if (!visited[v]) {
+					int new_distance = manhattan_distance(points[u], points[v]);
+					if (new_distance < heap_dict[v]) {
+						heap_dict[v] = new_distance;
+						min_heap.push({ new_distance, v });
+					}
+				}
+			}
+		}
+
+		return mst_weight;
+	}
+};
+
+class Solution {
+public:
+	int minCostConnectPoints(vector<vector<int>>& points) {
+		int n = points.size();
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap; //creating heap to store minimum weight
+		vector<bool> arr(n);
+		heap.push({ 0, 0 });
+		int mincost = 0;
+		int remaning = 0;
+		while (remaning < n) {
+			pair<int, int> topElement = heap.top();
+			heap.pop();
+
+			int weight = topElement.first;
+			int currNode = topElement.second;
+			if (arr[currNode]) {
+				continue;
+			}
+
+			arr[currNode] = true;
+			mincost += weight;
+			remaning++;
+
+			for (int nextNode = 0; nextNode < n; ++nextNode) {
+				if (!arr[nextNode]) {
+					int nextWeight = abs(points[currNode][0] - points[nextNode][0]) +
+						abs(points[currNode][1] - points[nextNode][1]);
+
+					heap.push({ nextWeight, nextNode });
+				}
+			}
+		}
+
+		return mincost;
+	}
+};
+
+//https://leetcode.com/problems/min-cost-to-connect-all-points/solutions/1982888/prim-s-algorithm-cpp-solution-with-explanation/
+
+//https://kbj96.tistory.com/15
+
+//https://8iggy.tistory.com/159
