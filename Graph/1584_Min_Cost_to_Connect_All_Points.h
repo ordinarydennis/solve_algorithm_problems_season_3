efddf4da56582f78976965ac918a6ec7d47ff205
ordinarydennis <ordinarydennis@gmail.com>
2024-01-27 23:28:54 +0900

    Solve LeetCode Problem - Graph - 1584. Min Cost to Connect All Points(try again)

--- Graph/1584_Min_Cost_to_Connect_All_Points.h
+++ Graph/1584_Min_Cost_to_Connect_All_Points.h
@@ -134,3 +134,59 @@ public:
 //https://kbj96.tistory.com/15
 
 //https://8iggy.tistory.com/159
+
+
+
+class Solution {
+public:
+	int minCostConnectPoints(vector<vector<int>>& points) {
+
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; 
+
+		int count = 0;
+
+		int size = static_cast<int>(points.size());
+
+		vector<bool> visit(points.size());
+
+		pq.push(pair<int, int> {0, 0});
+
+		int ret = 0;
+
+		int remain = size;
+
+		while (remain)
+		{
+			count++;
+
+			auto p = pq.top();
+
+			pq.pop();
+
+			int w = p.first;
+			int nodeIndex = p.second;
+
+			if (visit[nodeIndex])
+				continue;
+
+			ret += w;
+
+			remain--;
+
+			visit[nodeIndex] = true;
+
+			for (int i = 0; i < size; i++)
+			{
+				if (visit[i])
+					continue;
+
+				int d = abs(points[i][0] - points[nodeIndex][0]) +
+					abs(points[i][1] - points[nodeIndex][1]);
+
+				pq.push(pair<int, int> {d, i});
+			}	
+		}
+
+		return ret;
+	}
+};
