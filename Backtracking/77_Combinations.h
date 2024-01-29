b82eda7cc29cbb9133ac76f9b0bcd10fa30d5fb9
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-01-29 14:26:28 +0900

    Solve LeetCode Problem - Backtracking - 77. Combinations(read solution, solved agrain) https://leetcode.com/problems/combinations/submissions/1159766124/

--- Backtracking/77_Combinations.h
+++ Backtracking/77_Combinations.h
@@ -35,3 +35,35 @@ public:
 		return ret;
 	}
 };
+
+class Solution {
+public:
+	vector<vector<int>> ans;
+
+	void helper(int idx, int k, vector<int>& current, int n)
+	{
+		if (current.size() == k)    // base case
+		{
+			ans.push_back(current);
+			return;
+		}
+
+		for (int i = idx;i < n + 1;i++)
+		{
+			current.push_back(i);  //consider the current element i
+			helper(i + 1, k, current, n); // generate combinations
+			current.pop_back(); //proceed to next element
+		}
+	}
+
+	vector<vector<int>> combine(int n, int k) {
+		vector<int>current;
+		helper(1, k, current, n);
+		return ans; //return answer
+	}
+};
+
+//https://leetcode.com/problems/combinations/submissions/1159766124/
+
+//backtracking
+//https://youngdroidstudy.tistory.com/entry/%EB%B0%B1%ED%8A%B8%EB%9E%98%ED%82%B9-%EB%B0%B1%ED%8A%B8%EB%9E%98%ED%82%B9%EC%9D%98-%EC%84%A4%EB%AA%85%EA%B3%BC-%EA%B0%84%EB%8B%A8%ED%95%9C-%EC%98%88%EC%A0%9C%ED%92%80%EC%9D%B4
