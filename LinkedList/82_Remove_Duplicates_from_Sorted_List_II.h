764404d3ba43890dfe5fa2cdd6c4129549ccb500
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-02-01 22:05:32 +0900

    Solve LeetCode Problem - LinkedList - 82. Remove Duplicates from Sorted List II(read solution) https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/solutions/1395724/8-lines-c-solution-faster-than-100-submissions-for-beginners-explanation-of-approach/
    https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/solutions/816387/simple-3-pointer-solution-8ms-c/

--- LinkedList/82_Remove_Duplicates_from_Sorted_List_II.h
+++ LinkedList/82_Remove_Duplicates_from_Sorted_List_II.h
@@ -65,4 +65,65 @@ public:
         
         return ret;
     }
+
+	ListNode* deleteDuplicates(ListNode* head) {
+
+		ListNode dummy(0, head);
+
+		ListNode* previous = &dummy, * current = head;
+
+		while (current) {
+			ListNode* next_node = current->next;
+			while (next_node && current->val == next_node->val) {
+				ListNode* temp = next_node->next;
+				delete next_node;
+				next_node = temp;
+			}
+
+			if (current->next == next_node) {
+				previous = current;
+			}
+			else {
+				previous->next = next_node;
+				delete current;
+			}
+			current = next_node;
+		}
+
+		return dummy.next;
+	}
 };
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/solutions/1395724/8-lines-c-solution-faster-than-100-submissions-for-beginners-explanation-of-approach/
+class Solution
+{
+public:
+	ListNode* deleteDuplicates(ListNode* head)
+	{
+		if (!head)
+			return head;
+
+		ListNode* prev = NULL, * curr = head, * next = curr->next;
+		while (next)
+		{
+			if (curr->val == next->val)
+			{
+				while (next && next->val == curr->val)
+					next = next->next;
+				if (!prev) // This condition means element at the head is repeating. So, head pointer needs to be shifted.
+					head = next;
+				else
+					prev->next = next;
+			}
+			else
+			{
+				prev = curr;
+			}
+
+			curr = next;
+			if (next)
+				next = curr->next;
+		}
+		return head;
+	}
+};
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/solutions/816387/simple-3-pointer-solution-8ms-c/
