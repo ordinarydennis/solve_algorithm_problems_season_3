a62246e34678027105a7e2c9011309247c4455fd
ordinarydennis <ordinarydennis@gmail.com>
2024-02-04 23:10:26 +0900

    Solve LeetCode Problem - Trie - 208. Implement Trie (Prefix Tree)(read solution, try again) https://leetcode.com/problems/implement-trie-prefix-tree/solutions/3180933/solution/

--- Trie/208_Implement_Trie_(Prefix_Tree).h
+++ Trie/208_Implement_Trie_(Prefix_Tree).h
@@ -1,3 +1,4 @@
+//use recursion
 class Trie {
 
 private:
@@ -5,107 +6,80 @@ private:
 	class Node {
 
 	public:
-		string val;
-		Node* child['z' - 'a'] = { nullptr, };
+		bool mIsWord = false;
+		Node* mChild['z' - 'a' + 1] = { nullptr, };
 
 	};
 
 
-public
-	:
-	Trie() {
-		mNode = new Node();
+public:
+
+	void insert(string word)
+	{
+		insert(&mNode, word, 0);
 	}
-
-	void insert(string word) {
-
-		if (word.empty())
-		{
-			return;
-		}
-
-		insert(mNode, word, 0);
-
-	}
-
-	bool search(string word) {
-
-		return search(mNode, word, 0);
-
-	}
-
-	bool startsWith(string prefix) {
-		
-		return startWith(mNode, prefix, 0);
-
-	}
-
-private:
-
+	
 	void insert(Node* node, string& word, int index)
 	{
-		auto* chNode = node->child[word[index]];
+		if(word.size() == index)
+		{
+			node->mIsWord = true;
+			return;	
+		}
+
+		auto* chNode = node->mChild[word[index] - 'a'];
 
 		if (nullptr == chNode)
 		{
 			chNode = new Node();
+			node->mChild[word[index] - 'a'] = chNode;
 		}
 
-		if (word.size() - 1 == index)
-		{
-			chNode->val = word;
-			return;
-		}
-		else
-		{
-			insert(chNode, word, index + 1);
-		}
-		
+		insert(chNode, word, index + 1);
+	}
+
+	bool search(string word) {
+
+		return search(&mNode, word, 0);
+
 	}
 
 	bool search(Node* node, string& word, int index)
 	{
-		auto* chNode = node->child[word[index]];
+		if(word.size() == index)
+			return node->mIsWord;
+
+		auto* chNode = node->mChild[word[index] - 'a'];
 
 		if (nullptr == chNode)
-		{
 			return false;
-		}
 
-		if (word.size() - 1 == index)
-		{
-			return chNode->val == word;
-		}
+		return search(chNode, word, index + 1);
+	}
 
-		else
-		{
-			return search(chNode, word, index + 1);
-		}
+
+	bool startsWith(string prefix) {
 		
+		return startWith(&mNode, prefix, 0);
+
 	}
 
 	bool startWith(Node* node, string& prefix, int index)
 	{
-		auto* c = node->child[prefix[index]];
-
-		if (nullptr == c)
-		{
-			return false;
-		}
-
-		if (prefix.size() - 1 == index)
-		{
+		if(prefix.size() == index)
 			return true;
-		}
-		else
-		{
-			return startWith(c, prefix, index + 1);
-		}
+
+		auto* cNode = node->mChild[prefix[index] - 'a'];
+
+		if (nullptr == cNode)
+			return false;
+	
+		return startWith(cNode, prefix, index + 1);
 	}
 
 
 private:
-	Node* mNode = nullptr;
+	Node mNode;
 
 };
 
@@ -116,3 +90,66 @@ private:
  * bool param_2 = obj->search(word);
  * bool param_3 = obj->startsWith(prefix);
  */
+
+
+//use loop
+class Trie {
+
+private:
+
+	class Node {
+
+	public:
+		bool mIsWord = false;
+		Node* mChild['z' - 'a' + 1] = { nullptr, };
+
+	};
+
+
+public:
+
+	void insert(string word) {
+
+		Node* node = &mNode;
+
+		for(auto c : word)
+		{
+			if (nullptr ==  node->mChild[c - 'a'])
+				node->mChild[c - 'a'] = new Node();
+
+			 node =  node->mChild[c - 'a'];
+		}
+
+		node->mIsWord = true;
+
+	}
+
+	bool search(string word, bool isPrefix = false) {
+
+		Node* node = &mNode;
+
+		for(auto c : word)
+		{
+			if (nullptr == node->mChild[c - 'a'])
+			{
+				return false;
+			}
+
+			node = node->mChild[c - 'a'];
+		}
+
+		return (isPrefix) ? true :  node->mIsWord;
+	}
+
+	bool startsWith(string prefix) {
+		
+		return search(prefix, true);
+	}
+
+
+private:
+	Node mNode;
+
+};
+
+//https://leetcode.com/problems/implement-trie-prefix-tree/solutions/3180933/solution/
