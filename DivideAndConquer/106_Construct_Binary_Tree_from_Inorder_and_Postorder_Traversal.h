71b61992dafc8faa0a6d5916f735f1ba37ddbf65
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-02-08 22:52:41 +0900

    Solve LeetCode Problem - Divide and Quancur - 106. Construct Binary Tree from Inorder and Postorder Traversal(read solution) https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/solutions/3302159/easy-solutions-in-java-python-and-c-look-at-once/

--- DivideAndConquer/106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.h
+++ DivideAndConquer/106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.h
@@ -62,3 +62,47 @@ public:
 		return  root;
     }
 };
+
+class Solution {
+public:
+	TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+		unordered_map<int, int> index;
+		for (int i = 0; i < inorder.size(); i++) {
+			index[inorder[i]] = i;
+		}
+		return buildTreeHelper(inorder, postorder, 0, inorder.size() - 1, 0, postorder.size() - 1, index);
+	}
+
+	TreeNode* buildTreeHelper(vector<int>& inorder, vector<int>& postorder, int inorderStart, int inorderEnd, int postorderStart, int postorderEnd, unordered_map<int, int>& index) {
+		if (inorderStart > inorderEnd || postorderStart > postorderEnd) {
+			return nullptr;
+		}
+		int rootVal = postorder[postorderEnd];
+		TreeNode* root = new TreeNode(rootVal);
+		int inorderRootIndex = index[rootVal];
+		int leftSubtreeSize = inorderRootIndex - inorderStart;
+		root->left = buildTreeHelper(inorder, postorder, inorderStart, inorderRootIndex - 1, postorderStart, postorderStart + leftSubtreeSize - 1, index);
+		root->right = buildTreeHelper(inorder, postorder, inorderRootIndex + 1, inorderEnd, postorderStart + leftSubtreeSize, postorderEnd - 1, index);
+		return root;
+	}
+};
+
+//https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/solutions/3302159/easy-solutions-in-java-python-and-c-look-at-once/
+
+
+root node of the left sub tree is the last element between begin and end of postorder arr
+the range of left sub tree in postorder arr is from begin to rootIndexIn - 1 of postorder arr
+
+Input : inorder = [8, 9, 10, 3, 15, 20, 7], postorder = [8, 10, 9, 15, 7, 20, 3]
+
+                   3
+inorder    8 9 10     15 20 7
+				   
+postorder  8 10 9     15 7 20   3
+the search range of postorder arr IN next sub tree is from start of post orderr arr to start + leftSubtreeSize - 1 IN inorder arr
+What is the last element of postorder arr?
+It is root node of the inorder arr
+so, What is the first element of postorder arr?
+It is most left node of the inorder arr.
+
+	
