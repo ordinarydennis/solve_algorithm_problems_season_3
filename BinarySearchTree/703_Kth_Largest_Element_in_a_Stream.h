61159910b4da13edf71a8160d8a7548647f1c186
ordinarydennis <ordinarydennis@gmail.com>
2024-02-17 22:56:04 +0900

    Solve LeetCode Problem - Binary Search Tree - 703. Kth Largest Element in a Stream(read solution and solved again) https://leetcode.com/problems/kth-largest-element-in-a-stream/solutions/596093/c-solution-with-understandable-explaination/

--- BinarySearchTree/703_Kth_Largest_Element_in_a_Stream.h
+++ BinarySearchTree/703_Kth_Largest_Element_in_a_Stream.h
@@ -25,3 +25,40 @@ private:
 	int								mK = 0;
 
 };
+
+
+class KthLargest {
+
+public:
+	KthLargest(int k, vector<int>& nums) {
+
+		m_k = k;
+		for (int x : nums)
+			add(x);
+	}
+
+	int add(int val) {
+		//the queue maintains only m_k values 
+		//so that the top element is kth largest element.
+		if (minheap.size() < m_k)
+			minheap.push(val);
+		else
+		{
+			//if the value added is bigger than top element,
+			//pop the top element and add val value.
+			if (val > minheap.top())
+			{
+				minheap.pop();
+				minheap.push(val);
+			}
+		}
+		return minheap.top();
+	}
+
+private:
+	priority_queue<int, vector<int>, greater<int>> minheap;
+	int m_k;
+
+};
+
+//https://leetcode.com/problems/kth-largest-element-in-a-stream/solutions/596093/c-solution-with-understandable-explaination/
