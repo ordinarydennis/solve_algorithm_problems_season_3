2bf6e71c7c34f64c35843fca523d17f30810d880
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-02-19 14:07:25 +0900

    Solve LeetCode Problem - Array - 455. Assign Cookies(read solution) https://leetcode.com/problems/assign-cookies/solutions/4485308/o-n-log-n-time-o-1-space-c-java-python-js-explained-sorting/

--- Array/455_Assign_Cookies.h
+++ Array/455_Assign_Cookies.h
@@ -2,32 +2,24 @@ class Solution {
 public:
 	int findContentChildren(vector<int>& g, vector<int>& s) {
 
-
-		std::sort(g.begin(), g.end());
-		std::sort(s.begin(), s.end());
+		std::sort(g.begin(), g.end(), std::greater<int>());
+		std::sort(s.begin(), s.end(), std::greater<int>());
 
 		int ret = 0;
 
 		int i = 0;
 		int j = 0;
 
-		for (; i < static_cast<int>(s.size()); )
+		while (j < static_cast<int>(g.size()) && i < static_cast<int>(s.size()))
 		{
-			if (static_cast<int>(g.size()) <= j)
-				break;
-
-			if (static_cast<int>(g.size()) <= i)
-				break;
-
 			if (s[i] >= g[j])
-			{
-				j++;
-				ret++;
-				s[i]--;
-			}
-			i++;
+				i++, ret++;
+
+			j++;
 		}
 
 		return ret;
 	}
 };
+
+//https://leetcode.com/problems/assign-cookies/solutions/4485308/o-n-log-n-time-o-1-space-c-java-python-js-explained-sorting/
