c484772919ee02f8516dcb2b523dc7feaa947b8b
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-02-29 13:50:49 +0900

    Solve LeetCode Problem - Sort - 148. Sort List(read solution) https://leetcode.com/problems/sort-list/solutions/166324/python-c-c-java-legit-iterative-solutions-o-1-space-no-recursion-with-detailed-explaination/

--- Sort/148_Sort_List.h
+++ Sort/148_Sort_List.h
@@ -27,3 +27,32 @@ public:
 		}
 	}
 };
+
+
+class Solution {
+public:
+	ListNode* sortList(ListNode* head, ListNode* tail = nullptr)
+	{
+		if (head != tail) {
+			ListNode* smaller;
+			ListNode** smaller_next = &smaller;
+			for (ListNode** prev = &head->next; *prev != tail; ) {
+				if (head->val > (**prev).val) {
+					*smaller_next = *prev;
+					smaller_next = &((**smaller_next).next);
+					*prev = (**prev).next;
+				}
+				else {
+					prev = &((**prev).next);
+				}
+			}
+
+			*smaller_next = head;
+			head->next = sortList(head->next, tail);
+			head = sortList(smaller, head);
+		}
+		return head;
+	}
+};
+
+//https://leetcode.com/problems/sort-list/solutions/166324/python-c-c-java-legit-iterative-solutions-o-1-space-no-recursion-with-detailed-explaination/
