afdb245beaeeb10bca54858e0f158afe751bd8df
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-03-06 13:59:23 +0900

    Solve LeetCode Problem - DP - 70. Climbing Stairs(read solution) https://leetcode.com/problems/climbing-stairs/solutions/3708750/4-method-s-beat-s-100-c-java-python-beginner-friendly/

--- DP/70_Climbing_Stairs.h
+++ DP/70_Climbing_Stairs.h
@@ -1,4 +1,4 @@
-class Solution {
+﻿class Solution {
 
 	int ret = 0;
 
@@ -35,3 +35,44 @@ public:
 		return ret;
 	}
 };
+
+
+class Solution {
+
+	int dfs(int n)
+	{
+		if (n <= 1)
+			return 1;
+
+		return dfs(n - 1) + dfs(n - 2);
+	}
+
+
+public:
+	int climbStairs(int n) {
+
+		return dfs(n);
+	}
+};
+
+class Solution {
+
+
+public:
+	int climbStairs(int n) {
+
+		vector<int> memo(n + 1);
+
+		memo[0] = 1;
+		memo[1] = 1;
+
+		for (int i = 2; i <= n; i++)
+		{
+			memo[i] = memo[i - 1] + memo[i - 2];
+		}
+
+		return memo[n];
+	}
+};
+
+https://leetcode.com/problems/climbing-stairs/solutions/3708750/4-method-s-beat-s-100-c-java-python-beginner-friendly/
