f4180d315a5cda0396719b3e6958a288149f26e7
ordinarydennis <ordinarydennis@gmail.com>
2024-03-08 23:05:26 +0900

    Solve LeetCode Problem - DP - 118. Pascal's Triangle(read solution) https://leetcode.com/problems/pascals-triangle/solutions/4016203/three-approaches-beginner-friendly-full-explanation-c-java-python/

--- DP/118_Pascal's_Triangle.h
+++ DP/118_Pascal's_Triangle.h
@@ -31,3 +31,28 @@ public:
 
 	}
 };
+
+
+//Method 3: Using Dynamic Programming with 1D Array
+class Solution {
+public:
+	vector<vector<int>> generate(int numRows) {
+		vector<vector<int>> result;
+		vector<int> prevRow;
+
+		for (int i = 0; i < numRows; i++) {
+			vector<int> currentRow(i + 1, 1);
+
+			for (int j = 1; j < i; j++) {
+				currentRow[j] = prevRow[j - 1] + prevRow[j];
+			}
+
+			result.push_back(currentRow);
+			prevRow = currentRow;
+		}
+
+		return result;
+	}
+};
+
+//is it better than mine?
