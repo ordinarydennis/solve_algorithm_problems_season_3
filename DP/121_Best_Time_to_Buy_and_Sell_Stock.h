5e23e5912190af6b1411fa857b64facb13d939ea
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-03-11 13:56:42 +0900

    Solve LeetCode Problem - DP - 121. Best Time to Buy and Sell Stock(read solution) https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solutions/3169837/best-c-3-solution-dp-space-optimization-brute-force-optimize/

--- DP/121_Best_Time_to_Buy_and_Sell_Stock.h
+++ DP/121_Best_Time_to_Buy_and_Sell_Stock.h
@@ -1,4 +1,4 @@
-class Solution {
+﻿class Solution {
 public:
 	int maxProfit(vector<int>& prices) {
 
@@ -14,4 +14,103 @@ public:
 
 		return ret;
 	}
+};
+
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solutions/3169837/best-c-3-solution-dp-space-optimization-brute-force-optimize/
+/*
+
+    Time Complexity : O(N^2), Where N is the size of the Array(prices). As we check for possible pair, and the
+    total number of pairs are : N*(N–1)/2.
+
+    Space complexity : O(1), Constant space.
+
+    Using Array(Two Nested Loop). Brute Force Approach.
+
+    Note : This will give TLE.
+
+*/
+
+
+/***************************************** Approach 1 Code *****************************************/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size(), maxProfit = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (prices[j] - prices[i] > maxProfit) {
+                    maxProfit = prices[j] - prices[i];
+                }
+            }
+        }
+        return maxProfit;
+    }
+};
+
+
+
+
+
+
+/*
+
+    Time Complexity : O(N), As we iterate the array(prices) two times. Where N = size of the array.
+
+    Space complexity : O(N), Array(maxPrices) space.
+
+    Solved using Dynamic Programming Approach(tabulation).
+
+*/
+
+
+/***************************************** Approach 2 Code *****************************************/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> maxPrices(n, 0);
+        maxPrices[n - 1] = prices[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            maxPrices[i] = max(maxPrices[i + 1], prices[i]);
+        }
+        int maxProfit = 0;
+        for (int i = 0; i < n; i++) {
+            maxProfit = max(maxProfit, maxPrices[i] - prices[i]);
+        }
+        return maxProfit;
+    }
+};
+
+
+
+
+
+
+/*
+
+    Time Complexity : O(N), As we iterate the array(prices) two times. Where N = size of the array.
+
+    Space Complexity : O(1), Constant space.
+
+    Solved using Dynamic Programming Approach(Space optimisation).
+
+*/
+
+
+/***************************************** Approach 3 Code *****************************************/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int buyAtPrice = INT_MAX;
+        int maxProfit = 0;
+        for (int i = 0; i < n; i++) {
+            buyAtPrice = min(buyAtPrice, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - buyAtPrice);
+        }
+        return maxProfit;
+    }
 };
