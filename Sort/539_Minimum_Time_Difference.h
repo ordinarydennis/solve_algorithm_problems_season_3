3aa11eb78e8604abab23635c537fbadff321ba16
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-04-04 14:19:18 +0900

    Solve LeetCode Problem - Sort - 539. Minimum Time Difference(read solution) https://leetcode.com/problems/minimum-time-difference/solutions/4030795/c-o-nlogn-explained-easiest-approach-so-far/

--- Sort/539_Minimum_Time_Difference.h
+++ Sort/539_Minimum_Time_Difference.h
@@ -17,38 +17,58 @@ public:
 		std::vector<int> list;
 
 		for (auto& time : timePoints)
-		{
 			list.push_back(getMin(time));
-		}
 
 		std::sort(list.begin(), list.end());
 
 		int min = INT_MAX;
 
-		bool is0 = (0 == *list.begin());
-
 		for (int i = 1; i < list.size(); i++)
+			min = std::min(min, list[i] - list[i - 1]);
+
+		//because time is circulates, need to subtract between last to begin time to find minimum time gap.
+		return std::min(min, (1440 + list[0]) - list[list.size() - 1]);
+	}
+};
+
+//https://leetcode.com/problems/minimum-time-difference/solutions/4030795/c-o-nlogn-explained-easiest-approach-so-far/
+
+class Solution {
+public:
+	int findMinDifference(vector<string>& timePoints) {
+		vector<int>temp;
+
+		//CONVERSION TO MINUTES:-  TC= o(n)
+
+		for (int i = 0;i < timePoints.size();i++)
 		{
-			int a = list[i - 1];
-			int b = list[i];
-
-			if (0 == a)
-			{
-				min = std::min(min,
-					std::min(1440 - b, b - a)
-				);
-			}
-			else
-			{
-				min = std::min(min, b - a);
-			}
-
-			if (is0)
-			{
-				min = std::min(min, 1440 - b);
-			}
+			string s = timePoints[i];
+			int hour = stoi(s.substr(0, 2));
+			int minute = stoi(s.substr(3, 2));
+			int total = hour * 60 + minute;
+			temp.push_back(total);
 		}
 
-		return min;
+
+		//SORT THE VECTOR OF MINUTES:- TC = O(nlogn)
+
+		sort(temp.begin(), temp.end());
+
+
+		//CALCULATE THE  MIN. DIFFERENCE:-  TC = O(n)
+
+		int mini = INT_MAX;
+		int n = temp.size();
+		for (int i = 0;i < n - 1;i++)
+		{
+			int difference = temp[i + 1] - temp[i];
+			mini = min(difference, mini);
+
+		}
+
+		int last = (temp[0] + 1440) - temp[n - 1]; // 24hours *60 min. = 1440 min.
+		mini = min(last, mini);
+
+		return mini;
 	}
 };
