ecfacb017f1a213fed1c2a2074450b82a2e2a125
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-04-08 14:06:37 +0900

    Solve LeetCode Problem - String - 567. Permutation in String(read solution) https://leetcode.com/problems/permutation-in-string/editorial/ https://leetcode.com/problems/permutation-in-string/solutions/357821/c-concise-solution/

--- String/567_Permutation_in_String.h
+++ String/567_Permutation_in_String.h
@@ -76,3 +76,27 @@ public:
 		return isContain(s1, s2, p, visitSet);
 	}
 };
+
+https://leetcode.com/problems/permutation-in-string/solutions/357821/c-concise-solution/
+bool checkInclusion(string s1, string s2) {
+	vector<int> cur(26), goal(26);
+	
+	for (char c : s1) 
+		goal[c - 'a']++;
+
+	for (int i = 0; i < s2.size(); i++) {
+		
+		cur[s2[i] - 'a']++;
+
+		if (i >= s1.size()) 
+			cur[s2[i - s1.size()] - 'a']--;
+		
+		if (goal == cur) 
+			return true;
+	}
+	return false;
+}
+
+
+
+https://leetcode.com/problems/permutation-in-string/editorial/
