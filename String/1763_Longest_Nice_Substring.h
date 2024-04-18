2cc12450fae1094e09b1204d5d265bad95cdd208
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-04-18 14:34:58 +0900

    Solve LeetCode Problem - String - 1763. Longest Nice Substring(fail)

---
+++ String/1763_Longest_Nice_Substring.h
@@ -0,0 +1,53 @@
+class Solution {
+
+	bool isNice(string& s)
+	{
+		std::unordered_map<int, pair<bool, bool>> arr;
+
+		for (char c : s)
+		{
+			if ('a' <= c && c <= 'z')
+			{
+				arr[c - 'a'].first = true;
+			}
+			else
+			{
+				arr[c - 'A'].second = true;
+			}
+		}
+
+		for (auto& p : arr)
+		{
+			if (false == p.first || false == p.first)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+
+public:
+	string longestNiceSubstring(string s) {
+
+		string ret;
+
+		for (int i = 0; i < s.size() - 1; i++)
+		{
+			for (int l = 2; l <= s.size() - i; l++)
+			{
+				auto sub = s.substr(i, l);
+				if (isNice(sub))
+				{
+					if (ret.size() < l)
+					{
+						ret = sub;
+					}
+				}
+			}
+		}
+
+		return ret;
+	}
+};
