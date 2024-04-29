cc747eb8842d418863bbbcf91ffc216e862b2662
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-04-29 14:34:08 +0900

    Solve LeetCode Problem - Greedy - 253. Meeting Rooms II

---
+++ Greedy/253_Meeting_Rooms_II.h
@@ -0,0 +1,31 @@
+﻿class Solution {
+public:
+	int minMeetingRooms(vector<vector<int>>& intervals) {
+
+		std::sort(intervals.begin(), intervals.end());
+
+		int end = intervals[0][1];
+
+		int ret = 1;
+
+		for (int i = 1; i < intervals.size(); i++)
+		{
+			int s = intervals[i][0];
+			int e = intervals[i][1];
+
+			if (s < end)
+			{
+				ret++;
+				end = std::min(end, e);
+			}
+			else
+			{
+				end = e;
+			}
+			
+		}
+
+		return ret;
+
+	}
+};
