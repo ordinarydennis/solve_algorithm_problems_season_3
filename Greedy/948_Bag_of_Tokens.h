d69ee822dc45413857dbb8b0339339a0e744aae8
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-04-30 17:41:52 +0900

    Solve LeetCode Problem - Greedy - 948. Bag of Tokens(19m)

---
+++ Greedy/948_Bag_of_Tokens.h
@@ -0,0 +1,37 @@
+﻿class Solution {
+public:
+	int bagOfTokensScore(vector<int>& tokens, int power) {
+
+		sort(tokens.begin(), tokens.end());
+
+		int s = 0;
+		int e = tokens.size() - 1;
+
+		int score = 0;
+
+		while (s <= e)
+		{
+			if (tokens[s] <= power)
+			{
+				score++;
+				power -= tokens[s];
+				s++;
+			}
+			else
+			{
+				if (0 < score && 0 < e - s)
+				{
+					power += tokens[e];
+					e--;
+					score--;
+				}
+				else
+				{
+					s++;
+				}
+			}
+		}
+
+		return score;
+	}
+};
