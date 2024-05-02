c9d5a8d0bd1d57a9af7592d962bf3417ce69dc73
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-05-02 14:20:10 +0900

    Solve LeetCode Problem - Greedy - 2870. Minimum Number of Operations to Make Array Empty(13m)

---
+++ Greedy/2870_Minimum_Number_of_Operations_to_Make_Array_Empty.h
@@ -0,0 +1,39 @@
+﻿class Solution {
+public:
+	int minOperations(vector<int>& nums) {
+
+		std::unordered_map<int, int> memo;
+
+		for (int n : nums)
+		{
+			memo[n]++;
+		}
+
+		int ret = 0;
+
+		for (auto& [n , c] : memo)
+		{
+			if (c % 3 == 0)
+			{
+				ret += c / 3;
+			}
+			else if (3 < c)
+			{
+				if (c % 3 == 1 || c % 3 == 2)
+				{
+					ret += (c / 3) + 1;
+				}
+			}
+			else if (c % 3 == 2)
+			{
+				ret += 1;
+			}
+			else
+			{
+				return -1;
+			}
+		}
+
+		return ret;
+	}
+};
