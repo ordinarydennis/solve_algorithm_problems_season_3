71fc3263d6fc5a917b73a9f8fce696ab300a3cae
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-05-03 14:16:08 +0900

    Solve LeetCode Problem - Greedy - 1481. Least Number of Unique Integers after K Removals(14m)

---
+++ Greedy/1481_Least_Number_of_Unique_Integers_after_K_Removals.h
@@ -0,0 +1,37 @@
+﻿class Solution {
+public:
+	int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+
+		std::unordered_map<int, int> m;
+
+		for (int n : arr)
+		{
+			m[n]++;
+		}
+
+		auto l = [](vector<int>& a, vector<int>& b) {
+			return (a[0] > b[0]) || (a[0] == b[0] && a[1] > b[1]);
+		};
+
+		std::priority_queue <vector<int>, vector<vector<int>>, decltype(l)> q;
+
+		for (const auto& [n, count] : m)
+		{
+			q.push(vector<int> {count, n});
+		}
+		
+		for (int i = 0; i < k; i++)
+		{
+			auto v = q.top();
+
+			q.pop();
+
+			if (--v[0])
+			{
+				q.push(v);
+			}
+		}
+
+		return q.size();
+	}
+};
