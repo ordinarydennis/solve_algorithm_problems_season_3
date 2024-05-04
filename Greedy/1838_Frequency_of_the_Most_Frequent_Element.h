7f3be325e7c4a5bee257eb0178706aa1481209f4
ordinarydennis <ordinarydennis@gmail.com>
2024-05-04 19:09:23 +0900

    Solve LeetCode Problem - Greedy - 1838. Frequency of the Most Frequent Element https://leetcode.com/problems/frequency-of-the-most-frequent-element/editorial/

---
+++ Greedy/1838_Frequency_of_the_Most_Frequent_Element.h
@@ -0,0 +1,52 @@
+﻿class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+
+        std::unordered_map<int, int> m;
+
+        for (int n : nums)
+        {
+            m[n]++;
+        }
+
+        auto l = [](vector<int>& a, vector<int>& b) {
+
+            return a[0] < b[0] || (a[0] == b[0] && a[1] < b[1]);
+
+        };
+
+        std::priority_queue<vector<int>, vector<vector<int>>, decltype(l)> pq;
+
+        for (const auto& [n, count] : m)
+        {
+            pq.push({ count, n });
+        }
+
+        while (k && 1 < pq.size())
+        {
+            auto v1 = pq.top();
+
+            pq.pop();
+
+            auto v2 = pq.top();
+
+            pq.pop();
+
+            int d = v1[1] - v2[1];
+
+            if (d <= k)
+            {
+                k -= d;
+                pq.push({ v1[0] + v2[0], v1[1] });
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        auto r = pq.top();
+
+        return r[0];
+    }
+};
