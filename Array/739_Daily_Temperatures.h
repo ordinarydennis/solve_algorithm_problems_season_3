68ba5523d252592611859c039aa21fa73ad0744b
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-05-09 14:32:21 +0900

    Solve LeetCode Problem - Array - 739. Daily Temperatures https://leetcode.com/problems/daily-temperatures/editorial/

--- Array/739_Daily_Temperatures.h
+++ Array/739_Daily_Temperatures.h
@@ -1,5 +1,30 @@
 class Solution {
 public:
+	std::vector<int> dailyTemperatures(std::vector<int>& temperatures) {
+		int n = temperatures.size();
+		std::vector<int> answer(n, 0);  // Initialize all to 0
+		std::stack<int> stack;
+
+		for (int currDay = 0; currDay < n; currDay++) {
+			int currentTemp = temperatures[currDay];
+			// Pop until the current day's temperature is not
+			// warmer than the temperature at the top of the stack
+			while (!stack.empty() && temperatures[stack.top()] < currentTemp) {
+				int prevDay = stack.top();
+				stack.pop();
+				answer[prevDay] = currDay - prevDay;
+			}
+			stack.push(currDay);
+		}
+
+		return answer;
+	}
+};
+
+
+
+class Solution {
+public:
 	vector<int> dailyTemperatures(vector<int>& temperatures) {
 
 		vector<int> ret(temperatures.size());
