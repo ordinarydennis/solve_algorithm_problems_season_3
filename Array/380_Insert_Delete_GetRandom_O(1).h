5fe8b2b47c3e06976391555723f0e0ae9b141427
ordinarydennis <ordinarydennis@gmail.com>
2024-05-11 23:07:06 +0900

    Solve LeetCode Problem - Array - 380. Insert Delete GetRandom O(1) https://leetcode.com/problems/insert-delete-getrandom-o1/editorial/

--- Array/380_Insert_Delete_GetRandom_O(1).h
+++ Array/380_Insert_Delete_GetRandom_O(1).h
@@ -1,5 +1,65 @@
 class RandomizedSet {
 public:
+	RandomizedSet() {
+
+	}
+
+	bool insert(int val) {
+
+		auto it = m.find(val);
+
+		if (m.end() != it)
+		{
+			return false;
+		}
+
+		list.push_back(val);
+
+		return m.emplace(val, list.size() - 1).second;
+
+	}
+
+	bool remove(int val) {
+
+		auto it = m.find(val);
+
+		if (m.end() == it)
+		{
+			return false;
+		}
+
+		list[it->second] = list[list.size() - 1];
+
+		m[list[list.size() - 1]] = it->second;
+
+		list.pop_back();
+
+		return 0 < m.erase(val);
+	}
+
+	int getRandom() {
+
+		return list[rand() % list.size()];
+
+	}
+
+
+private:
+	std::vector<int> list;
+	std::unordered_map<int, int> m;
+
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
+
+class RandomizedSet {
+public:
     RandomizedSet() {
 
     }
