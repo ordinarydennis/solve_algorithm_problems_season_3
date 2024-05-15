bfc717195e89fb51f3464a57224872a15ce83574
ordinarydennis <ordinarydennis@gmail.com>
2024-05-15 22:59:38 +0900

    Solve LeetCode Problem - Array - 560. Subarray Sum Equals K https://leetcode.com/problems/subarray-sum-equals-k/editorial/

--- Array/560_Subarray_Sum_Equals_K.h
+++ Array/560_Subarray_Sum_Equals_K.h
@@ -2,6 +2,66 @@ class Solution {
 public:
 	int subarraySum(vector<int>& nums, int k) {
 
+		int n = nums.size();
+
+		int ret = 0;
+
+		for (int s = 0; s < n; s++)
+		{
+			for (int e = s + 1; e <= n; e++)
+			{
+				int sum = std::accumulate(nums.begin() + s, nums.begin() + e, 0);
+
+				if (sum == k)
+				{
+					ret++;
+				}
+			}
+		}
+
+		return ret;
+	}
+};
+
+
+class Solution {
+public:
+	int subarraySum(vector<int>& nums, int k) {
+
+		int n = nums.size();
+
+		std::vector<int> sum(n + 1);
+
+		for (int i = 1; i <= nums.size(); i++)
+		{
+			sum[i] = sum[i - 1] + nums[i - 1];
+		}
+
+		int ret = 0;
+
+		for (int s = 0; s < nums.size(); s++)
+		{
+			for (int e = s + 1; e <= nums.size(); e++)
+			{
+				if (sum[e] - sum[s] == k)
+				{
+					ret++;
+				}
+			}
+		}
+
+		return ret;
+	}
+
+};
+
+
+
+
+class Solution {
+public:
+	int subarraySum(vector<int>& nums, int k) {
+
 		int start = 0;
 
 		int sum = 0;
