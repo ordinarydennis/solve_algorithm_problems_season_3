53e407e079d0b03513bfd7b4a5fb77d5854f1f94
ordinarydennis <ordinarydennis@gmail.com>
2024-05-24 18:20:28 +0900

    Solve LeetCode Problem - DP - 1043. Partition Array for Maximum Sum https://leetcode.com/problems/partition-array-for-maximum-sum/editorial/

--- DP/1043_Partition_Array_for_Maximum_Sum.h
+++ DP/1043_Partition_Array_for_Maximum_Sum.h
@@ -1,3 +1,79 @@
+
+//bottom-up
+class Solution {
+public:
+	int maxSumAfterPartitioning(vector<int>& arr, int k) {
+
+		vector<int> dp(arr.size() + 1);
+
+		for (int start = arr.size() - 1; 0 <= start; start--)
+		{
+			int end = min(start + k, static_cast<int>(arr.size()));
+
+			int maxValue = 0;
+
+			for (int i = start; i < end; i++)
+			{
+				maxValue = max(maxValue, arr[i]);
+
+				dp[start] = max(
+					dp[start],
+					dp[i + 1] + maxValue * (i - start + 1)
+				);
+			}
+		}
+
+		return dp[0];
+	}
+};
+
+
+//top-down
+class Solution {
+
+	int dfs(vector<int>& dp, int start, vector<int>& arr, int k)
+	{
+		if (arr.size() <= start)
+		{
+			return 0;
+		}
+
+		if (-1 != dp[start])
+		{
+			return dp[start];
+		}
+
+		int end = min(start + k, static_cast<int>(arr.size()));
+
+		int maxValue = 0;
+
+		int ret = 0;
+
+		for (int i = start; i < end; i++)
+		{
+			maxValue = max(maxValue, arr[i]);
+
+			ret = max(
+				ret,
+				maxValue * (i - start + 1) + dfs(dp, i + 1, arr, k)
+			);
+		}
+
+		return dp[start] = ret;
+	}
+
+public:
+	int maxSumAfterPartitioning(vector<int>& arr, int k) {
+
+		vector<int> dp(arr.size(), -1);
+
+		return dfs(dp, 0, arr, k);
+
+	}
+};
+
+
+
 class Solution {
 
 	int dfs(int start, vector<int>& arr, int k)
