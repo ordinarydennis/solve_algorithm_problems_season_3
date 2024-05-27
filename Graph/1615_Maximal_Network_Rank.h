a0e87c20719e5bb4a0b2d971c1f40d3154359a30
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-05-27 20:04:08 +0900

    Solve LeetCode Problem - Graph - 1615. Maximal Network Rank

--- Graph/1615_Maximal_Network_Rank.h
+++ Graph/1615_Maximal_Network_Rank.h
@@ -1,38 +1,136 @@
 class Solution {
 
-	void dfs(std::unordered_map<int, std::vector<int>>& map, int v)
+	int GetLevel(int a, int b, unordered_map<int, vector<int>>& networks)
 	{
-		auto& r = map[v];
+		set<std::pair<int, int>> linkSet;
 
-		// v n
+		int level = 0;
 
-		for (int n : r)
+		for (const auto c : networks[a])
 		{
-
+			linkSet.insert(std::pair<int, int> {a, c});
+			linkSet.insert(std::pair<int, int> {c, a});
+			level++;
 		}
+		
+
+		for (const auto c : networks[b])
+		{
+			if (linkSet.count({ c, b }) || linkSet.count({ b, c }))
+			{
+				continue;
+			}
+
+			linkSet.insert(std::pair<int, int> {c, b});
+			linkSet.insert(std::pair<int, int> {b, c});
+
+			level++;
+		}
+
+		return level;
 	}
 
 
 public:
 	int maximalNetworkRank(int n, vector<vector<int>>& roads) {
 
-		std::unordered_map<int, std::vector<int>> map;
+		unordered_map<int, vector<int>> networks;
 
-		for (const auto& r : roads)
+		for (const auto& cities : roads)
 		{
-			map[r[0]].push_back(r[1]);
-			map[r[1]].push_back(r[0]);
+			networks[cities[0]].push_back(cities[1]);
+			networks[cities[1]].push_back(cities[0]);
 		}
 
-		for (const auto& road : roads)
+		int ret = 0;
+
+		for (const auto&  [c1, ne] : networks)
 		{
-			std::unordered_map<int, int> m;
-
-			// 0 , 1
-			for (int v : road)
+			for (int c2 : ne)
 			{
-
+				ret = max(ret, GetLevel(c1, c2, networks));
 			}
 		}
+
+		return ret;
 	}
 };
+
+class Solution {
+
+	int GetLevel(int a, int b, unordered_map<int, vector<int>> networks)
+	{
+		set<std::pair<int, int>> linkSet;
+
+		int level = 0;
+
+		for (const auto c : networks[a])
+		{
+			linkSet.insert(std::pair<int, int> {a, c});
+			linkSet.insert(std::pair<int, int> {c, a});
+			level++;
+		}
+
+
+		for (const auto c : networks[b])
+		{
+			if (linkSet.count({ c, b }) || linkSet.count({ b, c }))
+			{
+				continue;
+			}
+
+			linkSet.insert(std::pair<int, int> {c, b});
+			linkSet.insert(std::pair<int, int> {b, c});
+
+			level++;
+		}
+
+		return level;
+	}
+
+
+public:
+	int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+
+		unordered_map<int, vector<int>> networks;
+
+		for (const auto& cities : roads)
+		{
+			networks[cities[0]].push_back(cities[1]);
+			networks[cities[1]].push_back(cities[0]);
+		}
+
+		int ret = 0;
+
+		for (int c1 = 0; c1 < n - 1; c1++)
+		{
+			for (int c2 = 0; c2 < n; c2++)
+			{
+				ret = max(ret, GetLevel(c1, c2, networks));
+			}
+		}
+
+		return ret;
+	}
+};
+
+
+0 : 1
+1 : 0, 2
+2 : 1, 3, 4
+3 : 2
+4 : 2, 
+5 : 6, 7
+6 : 5
+7 : 5
+
+
+
+n =
+8
+roads =
+[[0, 1], [1, 2], [2, 3], [2, 4], [5, 6], [5, 7]]
+Output
+4
+Expected
+5
