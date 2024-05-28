008f1dcbc2088084d8bfd0b953222b81bfb85bcd
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2024-05-28 14:28:34 +0900

    Solve LeetCode Problem - Graph - 1615. Maximal Network Rank https://leetcode.com/problems/maximal-network-rank/editorial/

--- Graph/1615_Maximal_Network_Rank.h
+++ Graph/1615_Maximal_Network_Rank.h
@@ -1,4 +1,63 @@
 class Solution {
+public:
+	int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+
+		unordered_map<int, unordered_set<int>> map;
+
+		for (const auto& r : roads)
+		{
+			map[r[0]].insert(r[1]);
+			map[r[1]].insert(r[0]);
+		}
+
+		int ret = 0;
+
+		for (int s = 0; s < n; s++)
+		{
+			for (int e = s + 1 ; e < n; e++)
+			{
+				int level = map[s].size() + map[e].size();
+
+				if (map[s].count(e))
+				{
+					level--;
+				}
+				ret = std::max(ret, level);
+			}
+		}
+
+		return ret;
+	}
+};
+
+class Solution {
+public:
+	int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+		int maxRank = 0;
+		unordered_map<int, unordered_set<int>> adj;
+		// Construct adjency list 'adj', where adj[node] stores all nodes connected to 'node'.
+		for (auto& road : roads) {
+			adj[road[0]].insert(road[1]);
+			adj[road[1]].insert(road[0]);
+		}
+
+		// Iterate on each possible pair of nodes.
+		for (int node1 = 0; node1 < n; ++node1) {
+			for (int node2 = node1 + 1; node2 < n; ++node2) {
+				int currentRank = adj[node1].size() + adj[node2].size();
+				if (adj[node1].find(node2) != adj[node1].end()) {
+					--currentRank;
+				}
+				// Find the current pair's respective network rank and store if it's maximum till now.
+				maxRank = max(maxRank, currentRank);
+			}
+		}
+		// Return the maximum network rank.
+		return maxRank;
+	}
+};
+
+class Solution {
 
 	int GetLevel(int a, int b, unordered_map<int, vector<int>>& networks)
 	{
@@ -104,7 +163,7 @@ public:
 
 		for (int c1 = 0; c1 < n - 1; c1++)
 		{
-			for (int c2 = 0; c2 < n; c2++)
+			for (int c2 = c1 + 1; c2 < n; c2++)
 			{
 				ret = max(ret, GetLevel(c1, c2, networks));
 			}
